bae920b1a3daa68f4488aa9845f8dd0491c249f6
Shibili <shibilsbl007@gmail.com>
2025-10-04 12:13:13 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7739524779-10700",
+  "appId": "1:747903371651:web:bdfe8072833bf5b4920eb6",
+  "apiKey": "AIzaSyCsAmisOKAzhBhNspbLxeq0fUH8fQqZRNk",
+  "authDomain": "studio-7739524779-10700.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "747903371651"
+};

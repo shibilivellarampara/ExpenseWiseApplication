d34f1de117b0b23537a601baaf21c771a924d01a
Shibili <shibilsbl007@gmail.com>
2025-10-04 21:52:39 +0000

    there is a unwanted space in the right side of dashbord screen when opin

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -147,7 +147,7 @@ export default function DashboardPage() {
                                 <TabsTrigger value="year">Y</TabsTrigger>
                             </TabsList>
                         </CardHeader>
-                        <CardContent className="pl-2">
+                        <CardContent>
                              {isLoading ? (
                                 <Skeleton className="h-[350px] w-full" />
                             ) : (

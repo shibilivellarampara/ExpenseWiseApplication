0a56d9043842f472a91cbcfedc50222d93d1aa5b
Shibili <shibilsbl007@gmail.com>
2025-11-05 18:32:42 +0000

    your changes where colorrect but it applies only on dark theme that is w

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -15,7 +15,6 @@ const badgeVariants = cva(
         destructive:
           "border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80",
         outline: "text-foreground",
-        colorful: "border-transparent", // Ensures no default border interferes
       },
     },
     defaultVariants: {

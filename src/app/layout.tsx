ad85f65853bb43cd247d1098a7735293e1916f19
Shibili <shibilsbl007@gmail.com>
2025-12-04 13:16:25 +0000

    when im entering to app in iphone every time i need to login,is there an

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,6 +42,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
        <head>
         <meta name="mobile-web-app-capable" content="yes" />
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=Lexend:wght@300;400;500;600;700&display=swap" rel="stylesheet" />

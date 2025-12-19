6a436ffe780750aea76b62f444d2b1a272352870
Shibili <shibilsbl007@gmail.com>
2025-12-19 09:14:10 +0000

    currently the google sign in  in progress, so when clicking google show

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -189,22 +189,11 @@ export function LoginForm() {
 
 
     async function handleGoogleSignIn() {
-        setIsGoogleLoading(true);
-        if (!auth) {
-            toast({ variant: 'destructive', title: 'Error', description: 'Firebase is not configured correctly.' });
-            setIsGoogleLoading(false);
-            return;
-        }
-        const provider = new GoogleAuthProvider();
-         try {
-            await signInWithPopup(auth, provider);
-            toast({ title: 'Success!', description: 'You are now signed in with Google.', duration: 1000 });
-            router.push('/dashboard');
-        } catch (error: any) {
-            handleLoginError(error)
-        } finally {
-            setIsGoogleLoading(false);
-        }
+        toast({
+            variant: "default",
+            title: "Feature in progress",
+            description: "Google Sign-In is currently under development. Please use another sign-in method.",
+        });
     }
 
   async function handleForgotPassword() {
@@ -350,7 +339,7 @@ export function LoginForm() {
         </div>
       </div>
       <div className="grid grid-cols-1 gap-2">
-       <Button variant="outline" className="w-full h-9" onClick={handleGoogleSignIn} disabled={isLoading || isGoogleLoading || !auth}>
+       <Button variant="outline" className="w-full h-9" onClick={handleGoogleSignIn} disabled={isLoading || !auth}>
           {isGoogleLoading ? <Loader2 className="mr-2 h-4 w-4 animate-spin" /> : <svg className="mr-2 h-4 w-4" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="google" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512"><path fill="currentColor" d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 126 23.4 172.9 61.9l-76.2 74.8C307.7 99.8 280.7 86 248 86c-84.3 0-152.3 67.8-152.3 151.4s68 151.4 152.3 151.4c99.2 0 129.1-81.5 133.7-118.8H248v-94.2h239.1c2.3 12.7 3.9 26.1 3.9 40.2z"></path></svg>}
           Continue with Google
         </Button>

6cafac4909074613f4c85ff7880c16ea9cfbe361
Shibili <shibilsbl007@gmail.com>
2025-12-28 20:03:05 +0000

    starting build "8d7ee7af-f996-4aea-95b0-5b155e12a7b8" FETCHSOURCE BUILD

---
+++ workspace/src/app/(app)/transactions/[year]/[month]/page.tsx
@@ -0,0 +1,13 @@
+
+import { MonthlyExpensesClient } from '@/components/transactions/MonthlyExpensesClient';
+
+interface MonthlyExpensesPageProps {
+    params: {
+        year: string;
+        month: string;
+    };
+}
+
+export default function MonthlyExpensesPage({ params }: MonthlyExpensesPageProps) {
+  return <MonthlyExpensesClient year={params.year} month={params.month} />;
+}

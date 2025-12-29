e8fed429f595cc6f7c44d4d256c4774b3b494caa
Shibili <shibilsbl007@gmail.com>
2025-12-29 08:36:12 +0000

    "the loading page..." animation have issue, "loading finalncial dashboar

--- src/components/PageLoader.tsx
+++ src/components/PageLoader.tsx
@@ -4,7 +4,7 @@ import { AppLoader } from '@/components/AppLoader';
 
 export function PageLoader() {
   return (
-    <div className="flex h-[calc(100vh-10rem)] items-center justify-center">
+    <div className="flex h-screen w-full items-center justify-center">
       <AppLoader message="Loading page..." />
     </div>
   );

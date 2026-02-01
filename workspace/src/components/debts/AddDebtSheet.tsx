27de0b86901a1f83e7c0a13943b8313337001762
Shibili <shibilsbl007@gmail.com>
2026-02-01 10:44:15 +0000

    Try fixing this error: `Build Error: Ecmascript file had an error. Error

--- workspace/src/components/debts/AddDebtSheet.tsx
+++ workspace/src/components/debts/AddDebtSheet.tsx
@@ -1,3 +1,4 @@
+
 'use client';
 
 import {
@@ -43,7 +44,7 @@ interface AddDebtDialogProps {
     onOpenChange?: (open: boolean) => void;
 }
 
-const FloatingLabelInput = React.forwardRef<HTMLInputElement, InputProps &amp; { label: string }>(
+const FloatingLabelInput = React.forwardRef<HTMLInputElement, InputProps & { label: string }>(
     ({ className, label, id, ...props }, ref) => {
         const hasValue = props.value !== undefined && props.value !== null && String(props.value) !== '';
         return (
